3688aa9eecbb5c1d0479bce94eda8fb57b9e76a1
Ninoh-FOX <ninohfox@gmail.com>
2020-12-19 09:55:10 +0100

    rogue 2.0 alpha

--- board/opendingux/package/gmenu2x/gmenu2x.mk
+++ board/opendingux/package/gmenu2x/gmenu2x.mk
@@ -1,24 +1,29 @@
-#############################################################
+################################################################################
 #
 # gmenu2x
 #
-#############################################################
-GMENU2X_VERSION = c6e2883
-GMENU2X_SITE = $(call github,opendingux,gmenu2x,$(GMENU2X_VERSION))
+################################################################################
+GMENU2X_VERSION = master
+GMENU2X_SITE = $(call github,Ninoh-FOX,gmenu2x,$(GMENU2X_VERSION))
 GMENU2X_DEPENDENCIES = sdl sdl_ttf sdl_gfx dejavu libpng fonts-droid
-GMENU2X_CONF_OPTS = -DBIND_CONSOLE=ON
+GMENU2X_AUTORECONF = YES
+GMENU2X_CONF_OPTS = \
+	--with-sdl-prefix=$(STAGING_DIR)/usr \
+	--enable-platform=$(BR2_PACKAGE_GMENU2X_PLATFORM) \
+	--enable-stick=$(BR2_PACKAGE_GMENU2X_STICK) \
+	--enable-battery=$(BR2_PACKAGE_GMENU2X_BATTERY)
 
-ifeq ($(BR2_PACKAGE_GMENU2X_SHOW_CLOCK),y)
-GMENU2X_CONF_OPTS += -DCLOCK=ON
-else
-GMENU2X_CONF_OPTS += -DCLOCK=OFF
-endif
+#ifeq ($(BR2_PACKAGE_GMENU2X_SHOW_CLOCK),y)
+#GMENU2X_CONF_OPTS += -DCLOCK=ON
+#else
+#GMENU2X_CONF_OPTS += -DCLOCK=OFF
+#endif
 
-ifeq ($(BR2_PACKAGE_GMENU2X_CPUFREQ),y)
-GMENU2X_CONF_OPTS += -DCPUFREQ=ON
-else
-GMENU2X_CONF_OPTS += -DCPUFREQ=OFF
-endif
+#ifeq ($(BR2_PACKAGE_GMENU2X_CPUFREQ),y)
+#GMENU2X_CONF_OPTS += -DCPUFREQ=ON
+#else
+#GMENU2X_CONF_OPTS += -DCPUFREQ=OFF
+#endif
 
 ifeq ($(BR2_PACKAGE_LIBOPK),y)
 GMENU2X_DEPENDENCIES += libopk
@@ -28,4 +33,4 @@ ifeq ($(BR2_PACKAGE_LIBXDGMIME),y)
 GMENU2X_DEPENDENCIES += libxdgmime
 endif
 
-$(eval $(cmake-package))
+$(eval $(autotools-package))
